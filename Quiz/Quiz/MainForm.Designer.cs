515ac04704f77250145ee251ba3955a98a25689c
Kyra4code <lucarioecharmrito@gmail.com>
2024-05-02 20:31:25 -0300

    zedamanga

---
+++ Quiz/Quiz/MainForm.Designer.cs
@@ -0,0 +1,372 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: aluno
+ * Date: 02/05/2024
+ * Time: 19:13
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Quiz
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button buttonA;
+		private System.Windows.Forms.Button buttonB;
+		private System.Windows.Forms.Button buttonC;
+		private System.Windows.Forms.Button buttonD;
+		private System.Windows.Forms.Button buttomReiniciar;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label labelD;
+		private System.Windows.Forms.Label labelC;
+		private System.Windows.Forms.Label labelB;
+		private System.Windows.Forms.Label labelA;
+		private System.Windows.Forms.Label labelPontos;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label labelPergunta;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.buttonB = new System.Windows.Forms.Button();
+			this.buttonC = new System.Windows.Forms.Button();
+			this.buttonD = new System.Windows.Forms.Button();
+			this.buttomReiniciar = new System.Windows.Forms.Button();
+			this.labelPontos = new System.Windows.Forms.Label();
+			this.buttonA = new System.Windows.Forms.Button();
+			this.labelA = new System.Windows.Forms.Label();
+			this.labelB = new System.Windows.Forms.Label();
+			this.labelC = new System.Windows.Forms.Label();
+			this.labelD = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.labelPergunta = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(31, 512);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(83, 71);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Enabled = false;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(120, 512);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(83, 71);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "2";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Enabled = false;
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.Location = new System.Drawing.Point(209, 512);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(83, 71);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "3";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Enabled = false;
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.Location = new System.Drawing.Point(298, 512);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(83, 71);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "4";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Enabled = false;
+			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button5.Location = new System.Drawing.Point(387, 512);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(91, 71);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "5";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this.button6.Enabled = false;
+			this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button6.Location = new System.Drawing.Point(484, 512);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(83, 71);
+			this.button6.TabIndex = 5;
+			this.button6.Text = "6";
+			this.button6.UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			this.button7.Enabled = false;
+			this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button7.Location = new System.Drawing.Point(573, 512);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(83, 71);
+			this.button7.TabIndex = 6;
+			this.button7.Text = "7";
+			this.button7.UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this.button8.Enabled = false;
+			this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button8.Location = new System.Drawing.Point(662, 512);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(83, 71);
+			this.button8.TabIndex = 7;
+			this.button8.Text = "8";
+			this.button8.UseVisualStyleBackColor = true;
+			// 
+			// button9
+			// 
+			this.button9.Enabled = false;
+			this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button9.Location = new System.Drawing.Point(751, 512);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(83, 71);
+			this.button9.TabIndex = 8;
+			this.button9.Text = "9";
+			this.button9.UseVisualStyleBackColor = true;
+			// 
+			// button10
+			// 
+			this.button10.Enabled = false;
+			this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button10.Location = new System.Drawing.Point(840, 512);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(83, 71);
+			this.button10.TabIndex = 9;
+			this.button10.Text = "10";
+			this.button10.UseVisualStyleBackColor = true;
+			// 
+			// buttonB
+			// 
+			this.buttonB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonB.Location = new System.Drawing.Point(12, 166);
+			this.buttonB.Name = "buttonB";
+			this.buttonB.Size = new System.Drawing.Size(83, 71);
+			this.buttonB.TabIndex = 11;
+			this.buttonB.Text = "B";
+			this.buttonB.UseVisualStyleBackColor = true;
+			this.buttonB.Click += new System.EventHandler(this.ButtonBClick);
+			// 
+			// buttonC
+			// 
+			this.buttonC.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonC.Location = new System.Drawing.Point(12, 243);
+			this.buttonC.Name = "buttonC";
+			this.buttonC.Size = new System.Drawing.Size(83, 71);
+			this.buttonC.TabIndex = 12;
+			this.buttonC.Text = "C";
+			this.buttonC.UseVisualStyleBackColor = true;
+			this.buttonC.Click += new System.EventHandler(this.ButtonCClick);
+			// 
+			// buttonD
+			// 
+			this.buttonD.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonD.Location = new System.Drawing.Point(12, 320);
+			this.buttonD.Name = "buttonD";
+			this.buttonD.Size = new System.Drawing.Size(83, 71);
+			this.buttonD.TabIndex = 13;
+			this.buttonD.Text = "D";
+			this.buttonD.UseVisualStyleBackColor = true;
+			this.buttonD.Click += new System.EventHandler(this.ButtonDClick);
+			// 
+			// buttomReiniciar
+			// 
+			this.buttomReiniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttomReiniciar.Location = new System.Drawing.Point(1102, 525);
+			this.buttomReiniciar.Name = "buttomReiniciar";
+			this.buttomReiniciar.Size = new System.Drawing.Size(141, 45);
+			this.buttomReiniciar.TabIndex = 14;
+			this.buttomReiniciar.Text = "Reiniciar";
+			this.buttomReiniciar.UseVisualStyleBackColor = true;
+			this.buttomReiniciar.Click += new System.EventHandler(this.ButtomReiniciarClick);
+			// 
+			// labelPontos
+			// 
+			this.labelPontos.AutoSize = true;
+			this.labelPontos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelPontos.Location = new System.Drawing.Point(977, 537);
+			this.labelPontos.Name = "labelPontos";
+			this.labelPontos.Size = new System.Drawing.Size(90, 25);
+			this.labelPontos.TabIndex = 16;
+			this.labelPontos.Text = "Pontos:0";
+			// 
+			// buttonA
+			// 
+			this.buttonA.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonA.Location = new System.Drawing.Point(12, 89);
+			this.buttonA.Name = "buttonA";
+			this.buttonA.Size = new System.Drawing.Size(83, 71);
+			this.buttonA.TabIndex = 10;
+			this.buttonA.Text = "A";
+			this.buttonA.UseVisualStyleBackColor = true;
+			this.buttonA.Click += new System.EventHandler(this.ButtonAClick);
+			// 
+			// labelA
+			// 
+			this.labelA.AutoSize = true;
+			this.labelA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelA.Location = new System.Drawing.Point(114, 114);
+			this.labelA.Name = "labelA";
+			this.labelA.Size = new System.Drawing.Size(23, 25);
+			this.labelA.TabIndex = 0;
+			this.labelA.Text = "?";
+			// 
+			// labelB
+			// 
+			this.labelB.AutoSize = true;
+			this.labelB.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelB.Location = new System.Drawing.Point(114, 191);
+			this.labelB.Name = "labelB";
+			this.labelB.Size = new System.Drawing.Size(23, 25);
+			this.labelB.TabIndex = 2;
+			this.labelB.Text = "?";
+			// 
+			// labelC
+			// 
+			this.labelC.AutoSize = true;
+			this.labelC.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelC.Location = new System.Drawing.Point(114, 268);
+			this.labelC.Name = "labelC";
+			this.labelC.Size = new System.Drawing.Size(23, 25);
+			this.labelC.TabIndex = 3;
+			this.labelC.Text = "?";
+			// 
+			// labelD
+			// 
+			this.labelD.AutoSize = true;
+			this.labelD.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelD.Location = new System.Drawing.Point(114, 345);
+			this.labelD.Name = "labelD";
+			this.labelD.Size = new System.Drawing.Size(23, 25);
+			this.labelD.TabIndex = 4;
+			this.labelD.Text = "?";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(839, 31);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(324, 360);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 5;
+			this.pictureBox1.TabStop = false;
+			// 
+			// labelPergunta
+			// 
+			this.labelPergunta.AutoSize = true;
+			this.labelPergunta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelPergunta.Location = new System.Drawing.Point(41, 20);
+			this.labelPergunta.Name = "labelPergunta";
+			this.labelPergunta.Size = new System.Drawing.Size(28, 25);
+			this.labelPergunta.TabIndex = 17;
+			this.labelPergunta.Text = "1.";
+			// 
+			// panel1
+			// 
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Controls.Add(this.labelPergunta);
+			this.panel1.Controls.Add(this.pictureBox1);
+			this.panel1.Controls.Add(this.buttonB);
+			this.panel1.Controls.Add(this.buttonC);
+			this.panel1.Controls.Add(this.buttonD);
+			this.panel1.Controls.Add(this.labelD);
+			this.panel1.Controls.Add(this.labelC);
+			this.panel1.Controls.Add(this.labelB);
+			this.panel1.Controls.Add(this.labelA);
+			this.panel1.Controls.Add(this.buttonA);
+			this.panel1.Enabled = false;
+			this.panel1.Location = new System.Drawing.Point(27, 37);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1216, 426);
+			this.panel1.TabIndex = 15;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1333, 597);
+			this.Controls.Add(this.labelPontos);
+			this.Controls.Add(this.buttomReiniciar);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.panel1);
+			this.Name = "MainForm";
+			this.Text = "Quiz";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ Quiz/Quiz/MainForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Quiz
+{
+	public partial class MainForm : Form
+	{
+		public MainForm() //método construtor
+		{
+			InitializeComponent();
+		}
+		//Variaves globais 
+		string respostacerta = "X";
+		int pontos = 0;
+		string nomeImagem = "interrogação.png";
+		
+		// alternativas
+		void ButtonAClick(object sender, EventArgs e)
+		{
+			if (respostacerta == "A")
+			{
+				pictureBox1.Load();
+				MessageBox.Show("Parabéns, você acertou!!!");
+				pontos++;
+				labelPontos.Text = "Pontos: " + pontos;
+			}
+			else
+			{
+				MessageBox.Show("Você errou!!!");
+			}
+			panel1.Enabled = false;
+		}
+		void ButtonBClick(object sender, EventArgs e)
+		{
+			if (respostacerta == "B")
+			{
+				pictureBox1.Load();
+				MessageBox.Show("Parabéns, você acertou!!!");
+				pontos++;
+				labelPontos.Text = "Pontos: " + pontos;
+			}
+			else
+			{
+				MessageBox.Show("Você errou!!!");
+			}
+			panel1.Enabled = false;
+		}
+		void ButtonCClick(object sender, EventArgs e)
+		{
+			if (respostacerta == "C")
+			{
+				pictureBox1.Load();
+				MessageBox.Show("Parabéns, você acertou!!!");
+				pontos++;
+				labelPontos.Text = "Pontos: " + pontos;
+			}
+			else
+			{
+				MessageBox.Show("Você errou!!!");
+			}
+			panel1.Enabled = false;
+		}
+		void ButtonDClick(object sender, EventArgs e)
+		{
+			if (respostacerta == "D")
+			{
+				pictureBox1.Load();
+				MessageBox.Show("Parabéns, você acertou!!!");
+				pontos++;
+				labelPontos.Text = "Pontos: " + pontos;
+			}
+			else
+			{
+				MessageBox.Show("Você errou!!!");
+			}
+			panel1.Enabled = false;
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			pictureBox1.Load("transferir.jpg");
+			labelPergunta.Text = "Qual a capital de Xique -Xique,BA?";
+			labelA.Text = "Panamá";
+			labelB.Text = "tocantins";
+			labelC.Text = "Zamaica";
+			labelD.Text = "Bolso do Zimbabuei";
+			respostacerta = "D";
+			nomeImagem = "bolsodozimbabwei.jpg";
+			
+			panel1.Enabled = true;
+			
+			button1.Enabled = false;
+			button2.Enabled = true;
+			
+		}
+		void ButtomReiniciarClick(object sender, EventArgs e)
+		{
+			Application.Restart();
+		}
+	}
+}
